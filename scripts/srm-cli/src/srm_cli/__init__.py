a94d82fff4b07fdc18c4ebe6f2325176d1d21349
Stefan Rimaila <stefan@rimaila.fi>
2026-09-18 20:59:28 +0300

    feat(cli): add normalmaps fix

--- scripts/srm-cli/src/srm_cli/__init__.py
+++ scripts/srm-cli/src/srm_cli/__init__.py
@@ -1,2 +1,17 @@
+from pprint import pprint
+from argparse import ArgumentParser
+
+import srm_cli.cmds as cmds
+
+
 def main() -> None:
-    print("Hello from srm-cli!")
+    parser = ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true')
+    sub = parser.add_subparsers(title='Subcommands')
+
+    cmds.normals.configure(sub.add_parser('normals'))
+
+    args = parser.parse_args()
+
+    if 'func' in args:
+        args.func(args)
---
+++ scripts/srm-cli/src/srm_cli/cmds/__init__.py
@@ -0,0 +1,3 @@
+from . import (normals)
+
+modules = [normals]
